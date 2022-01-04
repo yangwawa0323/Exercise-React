b0179aeebf7b20857f1dee480191cd2edf097b66
yangwawa0323 <yangwawa0323@163.com>
2022-01-04 17:29:33 +0800

    Exercise React demo

---
+++ src/Components/App.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react'
+import { Header, Footer } from './Layouts'
+import Exercises from './Exercises'
+import { muscles, exercises } from '../store.js'
+
+export default class extends Component {
+	state = {
+		exercises,
+		exercise: {}
+	}
+
+	getExercisesByMuscles() {
+		return Object.entries(
+			this.state.exercises.reduce((exercises, exercise) => {
+				const { muscles } = exercise
+
+				exercises[muscles] = exercises[muscles]
+					? [...exercises[muscles], exercise]
+					: [exercise]
+
+				return exercises
+			}, {})
+		)
+	}
+
+	handleCategorySelect = category => {
+		this.setState({
+			category
+		})
+	}
+
+	handleExerciseSelect = id => {
+		this.setState(({ exercises }) => ({
+			exercise: exercises.find(ex => ex.id === id)
+		}))
+	}
+
+	handleExerciseCreate = exercise => {
+		this.setState(({ exercises }) => ({
+			exercises: [
+				...exercises,
+				exercise
+			]
+		}))
+	}
+
+	render() {
+		const exercises = this.getExercisesByMuscles(),
+			{ category, exercise } = this.state
+
+		return <Fragment>
+			<Header
+				muscles={muscles}
+				onExerciseCreate={this.handleExerciseCreate}
+			/>
+
+			<Exercises
+				exercise={exercise}
+				category={category}
+				exercises={exercises}
+				onSelect={this.handleExerciseSelect}
+			/>
+
+			<Footer
+				category={category}
+				muscles={muscles}
+				onSelect={this.handleCategorySelect}
+			/>
+		</Fragment>
+	}
+}
